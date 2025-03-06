b8f869605adc28f24ae10418cfa07761fb4150be
emmanuel <entibimenya03@gmail.com>
2025-03-06 12:22:11 +0000

    building list of friends static part

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FriendsList from "./FriendsList";
+//import "./index.css";
+function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+export default App;
