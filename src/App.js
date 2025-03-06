16764c20135c38863f82c2020d55c103ed2fd24d
emmanuel <entibimenya03@gmail.com>
2025-03-06 14:31:07 +0000

    rendering showAddFriend dynamically using state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import FormAddFriend from "./FormAddFriend";
 import FriendsList from "./FriendsList";
 import Button from "./Button";
 import FormSplitBill from "./FormSplitBill";
+import { useState } from "react";
 function App() {
+  //display the form AddFriend conditionally
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {/*conditioanally showAddFrien*/}
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,11 @@
 //since we are going to need to use button component in
 //many places, let's create that component and specify children props
-function Button({ children }) {
-  return <butto className="button">{children}</butto>;
+//updating showAddFriend with onClick
+function Button({ children, onClick }) {
+  return (
+    <butto className="button" onClick={onClick}>
+      {children}
+    </butto>
+  );
 }
 export default Button;
