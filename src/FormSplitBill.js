fcd1f25cdf49f32dc3346144445af892f866a585
emmanuel <entibimenya03@gmail.com>
2025-03-10 11:25:06 +0000

    controlled elements and derived state on SplitBill form

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -1,17 +1,40 @@
 import Button from "./Button";
-
+import { useState } from "react";
 function FormSplitBill({ selectedFriend }) {
+  //controlled elements
+  //empty string b/se these are input text elements
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  //derived state
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name} </h2>
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
       <label>🕴Your Expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
       <label>👬 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      {/*here we only specify the value b/se it is disabled*/}
+      <input type="text" disabled value={paidByFriend} />
       <label> 🤑 Who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
